810bc75d034eced04aafe1a78ebbd96a20b08cd4
peterjc <p.j.a.cock@googlemail.com>
2011-10-12 23:47:21 +0100

    FLAG support for SAM

--- Bio/Sequencing/SamBam/__init__.py
+++ Bio/Sequencing/SamBam/__init__.py
@@ -3,13 +3,24 @@
 # This code is part of the Biopython distribution and governed by its
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
-"""Fairly low level API for working with SAM/BAM files.
+r"""Fairly low level API for working with SAM/BAM files.
 
 This is intended to be written in Pure Python (so that it will work
 under PyPy, Jython, etc) but will attempt to follow the pysam API
 somewhat (which is a wrapper for the samtools C API).
 
+The low level objects can be used as follows:
+
+>>> data = "frag_5022\t16\tNC_000913_bb\t1\t255\t36M1S\t*\t0\t0\tTCTATTCATTATCTCAATAGCTTTTCATTCTGACTGN\tMMMMMMMMMMMMMMKKKK##%')+.024JMMMMMMM!\tRG:Z:Solexa_test\n"
+>>> read = SamRead(data)
+>>> read.tid
+'frag_5022'
+>>> read.flag
+16
+
 """
+
+#TODO - Have a Flag class?
 
 class SamRead(object):
     """Represents a SAM/BAM entry, i.e. a single read.
@@ -36,7 +47,12 @@ class SamRead(object):
 
         Note that a potentially unexpected side effect of this is that
         a malformed entry (e.g. a non-numeric mapping position) may
-        not be detected unless accessed.
+        not be detected unless accessed:
+
+        >>> print read.flag
+        Traceback (most recent call last):
+        ...
+        ValueError: invalid literal for int() with base 10: '...'
 
         You can modify values too, this overrides any values parsed
         from the raw data and will be used if saving the record back
@@ -60,6 +76,19 @@ class SamRead(object):
         self._tid = value
     tid = property(fget = _get_tid, fset = _set_tid,
                    doc = "Template ID (read ID aka QNAME)")
+
+    def _get_flag(self):
+        try:
+            return self._flag
+        except AttributeError:
+            flag = int(self._data[1])
+            self._flag = flag
+            return flag
+    def _set_flag(self, value):
+        self._flag = value
+    flag = property(fget = _get_flag, fset = _set_flag,
+                    doc = "FLAG (integer representing bit field)")
+
 
 #TODO - BamRead class, a subclass where the data is decoded using struct
 
