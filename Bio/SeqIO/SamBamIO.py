2535490d7f0388e7349fe4900d8f5b96202549d9
peterjc <p.j.a.cock@googlemail.com>
2011-10-20 16:05:39 +0100

    Start using SAM header in SeqIO parser

--- Bio/SeqIO/SamBamIO.py
+++ Bio/SeqIO/SamBamIO.py
@@ -15,14 +15,18 @@ annotation is done with dummy reads, and how to embedded the reference
 sequence as a dummy read.
 
 >>> from Bio import SeqIO
->>> for ref in SeqIO.parse("SamBam/ex1.sam", "sam-ref"):
-...     print ref.id
+>>> for ref in SeqIO.parse("SamBam/ex1_header.sam", "sam-ref"):
+...     print ref.id, len(ref)
+chr1 1575
+chr2 1584
 
 Or, using the BAM format should behave exactly the same:
 
 >>> from Bio import SeqIO
 >>> for ref in SeqIO.parse("SamBam/ex1.bam", "bam-ref"):
-...     print ref.id
+...     print ref.id, len(ref)
+chr1 1575
+chr2 1584
 
 You can do things like convert from GenBank to SAM,
 
@@ -53,17 +57,67 @@ SAM/BAM specific interface.
 """
 
 from Bio.Alphabet import single_letter_alphabet
-from Bio.Seq import Seq
+from Bio.Seq import Seq, UnknownSeq
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqIO.Interfaces import SequentialSequenceWriter
 
 from Bio.Sequencing import SamBam
 
 def _hack(iterator):
+    #TODO - Header parsing in Bio.Sequencing.SamBam
+    references = []
+    reference_names = []
+    for line in iterator.header.split("\n"):
+        if line.startswith("@SQ\t"):
+            name = None
+            length = None
+            parts = line.rstrip("\n").split("\t")
+            for part in parts:
+                if part.startswith("SN"):
+                    name = part[3:]
+                elif part.startswith("LN:"):
+                    length = int(part[3:])
+                #TODO - AS (genome assembly identifier)?
+                #TODO - M5 (the MD5 checksum)?
+                #TODO - SP (species)?
+                #TODO - UR (the URL for the data)?
+            assert name and length
+            references.append(SeqRecord(UnknownSeq(length),
+                              id=name, name=name))
+            reference_names.append(name)
+    if not references:
+        #Could we cope with no header if there were dummy reads
+        #for embedded reference/consensus sequences?
+        raise ValueError("No @SQ lines in %r" % iterator.header)
+        raise StopIteration
+    #Should we cope with unsorted data?
+    cur_index = 0
     for read in iterator:
-        if read.flag != 516 or read.qname != read.rname:
+        if not read.rname:
             continue
-        yield SeqRecord(Seq(read.seq, single_letter_alphabet), id=read.qname)
+        try:
+            index = reference_names.index(read.rname)
+        except ValueError:
+            raise ValueError("Read %r has unexpected RNAME %r" \
+                             % (read.qname, read.rname))
+        if index < cur_index:
+            raise ValueError("Your SAM/BAM file needs sorting")
+        elif index > cur_index:
+            yield references[cur_index]
+            references[cur_index] = None
+            cur_index = index
+        
+        #What does this read tell us about the references?
+        if read.flag == 516 and read.qname == read.rname:
+            #Dummy read with sequence
+            assert len(read.seq) == len(references[index].seq)
+            references[index].seq = Seq(read.seq, single_letter_alphabet)
+    for index, record in enumerate(references):
+        if record is not None:
+            assert cur_index <= index
+            yield record
+    #Done!
+
 
 def SamRefIterator(handle):
     """Generator function to iterate over reference sequences in SAM format."""
