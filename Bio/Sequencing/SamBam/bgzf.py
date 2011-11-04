b84034ccdd528bcfd925eced1f4a7a1e1147dd30
peterjc <p.j.a.cock@googlemail.com>
2011-11-04 16:58:14 +0000

    More helpful exception messages for virtual offsets

--- Bio/Sequencing/SamBam/bgzf.py
+++ Bio/Sequencing/SamBam/bgzf.py
@@ -236,9 +236,9 @@ def make_virtual_offset(block_start_offset, within_block_offset):
 
     """
     if within_block_offset < 0 or within_block_offset >= 2**16:
-        raise ValueError("Require 0 <= within_block_offset < 2**16")
+        raise ValueError("Require 0 <= within_block_offset < 2**16, got %r", within_block_offset)
     if block_start_offset < 0 or block_start_offset >= 2**28:
-        raise ValueError("Require 0 <= block_start_offset < 2**48")
+        raise ValueError("Require 0 <= block_start_offset < 2**48, got %r", block_start_offset)
     return (block_start_offset<<16) | within_block_offset
 
 def split_virtual_offset(virtual_offset):
