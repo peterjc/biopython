f471c8f5dc19a4e34d1d111a518088889fc69730
peterjc <p.j.a.cock@googlemail.com>
2012-02-18 19:35:57 +0000

    Move SAM/BAM testing into unittest

--- Bio/Sequencing/SamBam/__init__.py
+++ Bio/Sequencing/SamBam/__init__.py
@@ -1755,134 +1755,6 @@ def _cross_check_header_refs(reads, header="", referencenames=None, referencelen
         references = _sam_header_to_ref_list(header)
     return header, references
 
-
-def _comp_float(a,b):
-    return repr(a)==repr(b) or a==b or abs(float(a)-float(b))<0.000001,
-
-def _test_misc():
-    print "Misc tests..."
-    import gzip
-    import os
-    import sys
-
-    def compare(a_iter, b_iter):
-        from itertools import izip_longest
-        assert a_iter.nreferences == b_iter.nreferences, \
-            "%r vs %r" % (a_iter.nreferences, b_iter.nreferences)
-        assert a_iter.references == b_iter.references, \
-            "%r vs %r" % (a_iter.references, b_iter.references)
-        assert a_iter.lengths == b_iter.lengths, \
-            "%r vs %r" % (a_iter.lengths, b_iter.lengths)
-        if a_iter.header and b_iter.header:
-            assert a_iter.header == b_iter.header, \
-                "%r vs %r" % (a_iter.header, b_iter.header)
-        for a, b in izip_longest(a_iter, b_iter):
-            assert b is not None, "Extra read in a: %r" % str(a)
-            assert a is not None, "Extra read in b: %r" % str(b)
-            assert a.qname == b.qname, "%r vs %r" % (a.qname, b.qname)
-            assert a.flag == b.flag, "%r vs %r" % (a.flag, b.flag)
-            assert a.rname == b.rname, "%r vs %r" % (a.rname, b.rname)
-            assert a.pos == b.pos, "%r vs %r" % (a.pos, b.pos)
-            assert a.mapq == b.mapq, "%r vs %r" % (a.mapq, b.mapq)
-            assert a.cigar == b.cigar, "%r vs %r" % (a.cigar, b.cigar)
-            assert a.mrnm == b.mrnm, "%r vs %r" % (a.mrnm, b.mrnm)                                                        
-            assert a.mpos == b.mpos, "%r vs %r" % (a.pos, b.pos)
-            assert a.isize == b.isize, "%r vs %r" % (a.isize, b.isize)
-            assert a.seq == b.seq, "%r vs %r" % (a.seq, b.seq)
-            assert a.qual == b.qual, "%r vs %r" % (a.qual, b.qual)
-            assert a.tags.keys() == b.tags.keys(), "%r vs %r" % (a.tags.keys(), b.tags.keys())
-            for key in a.tags:
-                assert a.tags[key][0] == b.tags[key][0], \
-                       "%s tag %s has codes %s vs %s" % \
-                       (a.qname, key, a.tags[key][0], b.tags[key][0])
-                if a.tags[key][0]=="f":
-                    assert str(a.tags[key][1]) == str(b.tags[key][1]) or \
-                           abs(a.tags[key][1] - b.tags[key][1]) < 0.00001, \
-                           "%s tag %s have %f vs %f" % \
-                           (a.qname, key, a.tags[key][1], b.tags[key][1])
-                elif a.tags[key][0]=="Bf":
-                    pass
-                else:
-                    assert a.tags[key][1] == b.tags[key][1], \
-                           "%s tag %s:%s has %s vs %s" % \
-                           (a.qname, key, a.tags[key][0], a.tags[key][1], b.tags[key][1])
-            #Float formating in tags is annoying...
-            #assert str(a) == str(b), "Reads disagree,\n%s\n%s\n" % (a,b)
-    compare(SamIterator(open("SamBam/tags.sam")),
-            BamIterator(open("SamBam/tags.bam", "rb")))
-    compare(SamIterator(open("SamBam/tags.sam")),
-            BamIterator(gzip.open("SamBam/tags.bam"), gzipped=False))
-    for read in SamIterator(open("SamBam/tags.sam")):
-        #TODO - Test API for getting tag values
-        tag = str(read).rstrip("\n").split("\t")[-1]
-        assert read.qname == "tag_" + tag, \
-               "%s vs tag of %s" % (read.qname, tag)
-    for read in BamIterator(open("SamBam/tags.bam", "rb")):
-        tag = str(read).rstrip("\n").split("\t")[-1]
-        assert read.qname.startswith("tag_" + tag[:5])
-        if read.qname == "tag_" + tag:
-            continue
-        if ":f:" in tag:
-            old = float(read.qname.split(":")[2])
-            new = float(tag.split(":")[2])
-            assert _comp_float(old, new), \
-                   "%s vs tag of %s" % (read.qname, tag)
-        elif ":B:" in tag:
-            assert read.qname.startswith("tag_" + tag[:5]), \
-                   "%s vs tag of %s" % (read.qname, tag)
-            if ":B:f," in read.qname:
-                old = tuple(float(v) for v in read.qname.split(":B:f,")[1].split(","))
-                new = tuple(float(v) for v in tag.split(":")[2][2:].split(","))
-                assert len(old) == len(new), \
-                       "Count mismatch %i vs %i in %s\n%s\n%s\n" \
-                       % (read.qname, len(old), len(new), old, new)
-                for a,b in zip(old, new):
-                    assert _comp_float(a, b), \
-                           "Mismatch in %s,\n%s\n%s" % (read.qname, old, new)
-            else:
-                #Integers
-                old = [int(v) for v in read.qname.split(":B:")[1][2:].split(",")]
-                new = [int(v) for v in tag.split(":")[2][2:].split(",")]
-                assert old == new, "Mismatch in read %s vs %s\n%r\n%r" % (read.qname, tag, old, new)
-        else:
-            assert read.qname == "tag_" + tag, \
-                   "Please check %s vs tag of %s" % (read.qname, tag)
-
-    try:
-        #This is in Python 2.6+, but we need it on Python 3
-        from io import BytesIO
-    except ImportError:
-        #Must be on Python 2.5 or older
-        from StringIO import StringIO as BytesIO
-    for sam_filename, bam_filename in [
-        ("SamBam/tags.sam", "SamBam/tags.bam"),
-        ("SamBam/ex1_header.sam", "SamBam/ex1_header.bam"),
-        ("SamBam/bins.sam", "SamBam/bins.bam"),
-        ]:
-        #BAM -> BAM
-        if not os.path.isfile(bam_filename):
-            continue
-        print "%s -> %s check..." % (bam_filename, bam_filename)
-        handle = BytesIO()
-        count = BamWriter(handle, BamIterator(open(bam_filename, "rb")),
-                          gzipped=False)
-        if handle.getvalue() != gzip.open(bam_filename).read():
-            sys.stderr.write("ERROR: Couldn't reproduce %s -> %s\n" \
-                             % (bam_filename, bam_filename))
-        #SAM -> BAM
-        if not os.path.isfile(sam_filename):
-            continue
-        print "%s -> %s check..." % (sam_filename, bam_filename)
-        handle = BytesIO()
-        count = BamWriter(handle, SamIterator(open(sam_filename)),
-                          gzipped=False)
-        if handle.getvalue() != gzip.open(bam_filename).read():
-            sys.stderr.write("ERROR: Couldn't reproduce %s -> %s\n" \
-                             % (sam_filename, bam_filename))
-        #Comparing SAM output is tricky due to float formatting...
-
-    print "Done"
-
 def _test():
     """Run the module's doctests (PRIVATE).
 
@@ -1897,7 +1769,6 @@ def _test():
         os.chdir(os.path.join("..", "..", "..", "Tests"))
         doctest.testmod()
         print "Done"
-        _test_misc()
         os.chdir(cur_dir)
         del cur_dir
     elif os.path.isdir(os.path.join("Tests")):
@@ -1906,7 +1777,6 @@ def _test():
         os.chdir(os.path.join("Tests"))
         doctest.testmod()
         print "Done"
-        _test_misc()
         os.chdir(cur_dir)
         del cur_dir
 
