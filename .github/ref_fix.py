2aabb5f5f8f5ea504abba12f7c26f111f88b24cc
Peter Cock <p.j.a.cock@googlemail.com>
2023-06-19 06:18:48 +0100

    Support stdin for converting RST links to :ref: style.
    Example usage:
    
    for f in *.tex; do echo "Converting $f to rst"; pandoc -f latex -t rst $f | ../../.github/ref_fix.py - > ${f%.*}.rst; done
    
    Trivial changes like this:
    
       `[sec:orchids] <#sec:orchids>`__
    
    are shortened to just:
    
        :ref:`sec:orchids`
    
    More importantly, handles cases like this:
    
        (see section `1.3 <#sec:doctest>`__)
    
    It discards the hard-coded section number (accurate only in the
    solo-file conversion), giving:
    
        (see section :ref:`sec:doctest`)

--- .github/ref_fix.py
+++ .github/ref_fix.py
@@ -1,23 +1,63 @@
 #!/usr/bin/env python
+"""Python script to change pandoc links to :ref: style.
+
+Input arguments are filenames (in situ), or - for stdin to stdout.
+"""
+
 import re
 import sys
 
 # re_link = re.compile(r"`\[(.+)\] <#(.+)>`__")
 re_link = re.compile(r"`\[([A-Za-z0-9_:\-.]+)\] <#([A-Za-z0-9_:\-.]+)>`__")
 
-assert re_link.findall(r"Chapter \ `[chapter:quick_start] <#chapter:quick_start>`__ before\n")
-assert re_link.findall(r"(see Section `[sec:appendix-handles] <#sec:appendix-handles>`__):")
-assert re_link.findall(r"Section `[sec:Bio.SeqIO-and-StringIO] <#sec:Bio.SeqIO-and-StringIO>`__):")
+assert re_link.findall(
+    r"Chapter \ `[chapter:quick_start] <#chapter:quick_start>`__ before\n"
+)
+assert re_link.findall(
+    r"(see Section `[sec:appendix-handles] <#sec:appendix-handles>`__):"
+)
+assert re_link.findall(
+    r"Section `[sec:Bio.SeqIO-and-StringIO] <#sec:Bio.SeqIO-and-StringIO>`__):"
+)
 assert re_link.findall(r"Figure `[fig:three_track_cl2] <#fig:three_track_cl2>`__.")
-assert list(re_link.finditer(r"in Chapter \ `[chapter:seq_annot] <#chapter:seq_annot>`__. This aims to"))
-assert len(list(re_link.finditer(r"functions (`[eq:OP] <#eq:OP>`__) and (`[eq:NOP] <#eq:NOP>`__)."))) == 2
+assert list(
+    re_link.finditer(
+        r"in Chapter \ `[chapter:seq_annot] <#chapter:seq_annot>`__. This aims to"
+    )
+)
+assert (
+    len(
+        list(
+            re_link.finditer(
+                r"functions (`[eq:OP] <#eq:OP>`__) and (`[eq:NOP] <#eq:NOP>`__)."
+            )
+        )
+    )
+    == 2
+)
 
 re_section = re.compile(r"`[0-9.]+ <#([A-Za-z0-9_:\-.]+)>`__")
 
-assert re_section.findall(r"the label’s color (used in Section `1.1.9 <#sec:gd_nice_example>`__).")
+assert re_section.findall(
+    r"the label’s color (used in Section `1.1.9 <#sec:gd_nice_example>`__)."
+)
+
 
 def fix_line(line):
-    # e.g. Chapter \ `[chapter:quick_start] <#chapter:quick_start>`__
+    r"""Edit an RST line (assumes links not split over multiple lines).
+
+    e.g. Chapter \ `[chapter:quick_start] <#chapter:quick_start>`__
+
+    More importantly, handles cases like this::
+
+        (see section `1.3 <#sec:doctest>`__)
+
+    It discards the hard-coded section number (accurate only in the
+    solo-file conversion), giving::
+
+        (see section :ref:`sec:doctest`)
+
+    """
     line = line.replace("\xa0\\ ", " ")
     for match in re_link.finditer(line):
         old = match.group()
@@ -25,23 +65,31 @@ def fix_line(line):
         assert ref == match.group(2), old
         new = r":ref:`%s`" % ref
         line = line.replace(old, new)
-        print("%s -> %s" % (old, new))
+        sys.stderr.write("%s -> %s\n" % (old, new))
     for match in re_section.finditer(line):
         old = match.group()
         ref = match.group(1)
         assert old.endswith("<#%s>`__" % ref), old
         new = r":ref:`%s`" % ref
         line = line.replace(old, new)
-        print("%s -> %s" % (old, new))
+        sys.stderr.write("%s -> %s\n" % (old, new))
     return line
 
+
 def fix_file(filename):
+    """Edit an RST file in-situ."""
     with open(filename) as handle:
         lines = list(handle)
     with open(filename, "w") as handle:
         for line in lines:
             handle.write(fix_line(line))
 
+
 for f in sys.argv[1:]:
-    sys.stderr.write("Fixing %s\n" % f)
-    fix_file(f)
+    if f == "-":
+        sys.stderr.write("Fixing links on stdin\n")
+        for line in sys.stdin:
+            sys.stdout.write(fix_line(line))
+    else:
+        sys.stderr.write("Fixing %s\n" % f)
+        fix_file(f)
